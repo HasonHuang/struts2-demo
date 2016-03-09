12a6f8a682f7dec9819e304d6f5aeb9825917a56
hasonHuang <258831020@qq.com>
2016-03-09 22:25:06 +0800

    建立Struts2项目

---
+++ src/main/java/com/hason/study/struts2/demo/action/LoginAction.java
@@ -0,0 +1,13 @@
+package com.hason.study.struts2.demo.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	private static final long serialVersionUID = -5167192297164575063L;
+
+	public String execute() throws Exception {
+		System.out.println("ª∂”≠ π”√struts2!");
+		return super.execute();
+	}
+}
